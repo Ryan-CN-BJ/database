fb4344cdd1e5b9165d59e415ce8992a4af3351d3
Ryan-CN-BJ <maorunjie2025@qq.com>
2026-05-20 17:05:14 +0800

    feat:student外键

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
 import initDB from "./model/sync.js"
-import { addAdmin,deleteAdmin } from "./services/adminService.js";
-
-
 
 async function main(){
     await initDB()
-    await addAdmin({
-        loginId: 'admin',
-        loginPassword: '123456',
-        name: '管理员'
-    })
-    await addAdmin({
-        loginId: 'admin2',
-        loginPassword: '123456',
-        name: '管理员2'
-    })
-    await deleteAdmin(2)
 }
 
 main()
--- src/mock/mockStudent.ts
+++ src/mock/mockStudent.ts
@@ -7,7 +7,7 @@ export default function mockStudent(){
             {
                 name:'@cname()',
                 birthday:'@date("yyyy-MM-dd")',
-                classId:'@integer(1,16)',
+                ClassId:'@integer(1,16)',
                 mobile:/\x31[3-9]\d{9}/,
                 sex:'@integer(0,1)',
             }
